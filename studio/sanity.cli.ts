eacdfe63acc10564e73cbd55aff404348a6f8605
fbaraujopvh-host <fb.araujopvh@gmail.com>
2026-06-14 02:07:42 -0400

    chore: add studioHost to sanity.cli.ts

---
+++ studio/sanity.cli.ts
@@ -0,0 +1,9 @@
+import { defineCliConfig } from 'sanity/cli';
+
+export default defineCliConfig({
+  api: {
+    projectId: 'tanjm5z6',
+    dataset: 'production',
+  },
+  studioHost: 'sol-semijoias',
+});
